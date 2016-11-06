0646844d36b83b611e5ed726261dbffc78b51bcc
Nathan <dobobobado@gmail.com>
2016-11-06 15:39:37 -0500

    Implemented the template for Plady's main page, put comment in index.js

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ module.exports = function () {
 
     /* GET home page. */
     router.get('/', (req, res, next) => {
+        // check if user is registered
+        // if not registered, render 'index'
+        // if registered, render 'homepage'
         res.render('index', {title: 'Plady'});
     });
 
