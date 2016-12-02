2c348ea7e199bfda87e9683af81517f5521f62ae
Johnny <someazndude07@gmail.com>
2016-12-02 17:30:54 -0500

    var -> const

--- models/session.js
+++ models/session.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function (sequelize, DataTypes) {
-    var Session = sequelize.define('Session', {
+    const Session = sequelize.define('Session', {
         data: DataTypes.STRING(50000),
         expires: DataTypes.DATE
     });
